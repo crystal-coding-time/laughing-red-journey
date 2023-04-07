4e40597ad938239b856c9624ca1e56236b9a122b
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2023-04-07 19:06:35 -0400

    added more CSS and building navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {AiTwotoneHome} from 'react-icons/ai'
+import {BsFillPersonFill} from 'react-icons/bs'
+import {RiServiceFill} from 'react-icons/ri'
+import{MdDesignServices} from 'react-icons/md'
+import {RiChatSmile2Fill} from 'react-icons/ri'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#'><AiTwotoneHome /></a>
+      <a href='#about'><BsFillPersonFill /></a>
+      <a href='#experience'><RiServiceFill /></a>
+      <a href='#services'><MdDesignServices /></a>
+      <a href='#contact'><RiChatSmile2Fill /></a>
+    </nav>
   )
 }
 
