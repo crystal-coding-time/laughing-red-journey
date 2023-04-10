59d8a4bfc891aef5a90ca2bc0438d552e0a72ae0
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2023-04-10 18:46:13 -0400

    adding to the portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,21 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/crystal-coding-time" className='btn' target='_blank'>Github</a>
+          <a href="https://github.com/crystal-coding-time" className="btn btn-primary" target='_blank'>Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
