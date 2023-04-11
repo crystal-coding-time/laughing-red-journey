2f3ed9f5de41d0d9eb3cd7d3d7860f70ae7da0fd
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2023-04-11 10:50:35 -0400

    adding to testiminials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './testimonials.css'
+import avatar00 from '../../assets/images/avatar00.JPG'
+import avatar01 from '../../assets/images/avatar01.JPG'
+import avatar02 from '../../assets/images/avatar02.JPG'
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Past Client</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className='testimonial'>
+          <div className="client__avatar">
+            <img src={avatar00} alt="client headshot" />
+            <h5 className='client__name'>Scott</h5>
+            <small className="client__review">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam, quas.</small>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
