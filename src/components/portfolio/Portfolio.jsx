ac1f768749f570753a8478fd0983bf96447a2607
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2023-04-10 23:09:33 -0400

    added dynamic creation

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/images/quizz.jpeg'
-import IMG2 from '../../assets/images/password.jpeg'
-import IMG3 from '../../assets/images/amature.jpeg'
+import data from './portfolio-array'
 
 const Portfolio = () => {
   return (
@@ -11,66 +9,23 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="Image of quizz project" />
-          </div>
-          <h3>JavaScript Quizz</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/crystal-coding-time" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com/crystal-coding-time" className="btn btn-primary" target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="Image of Random Password Generator" />
-          </div>
-          <h3>Random Password Generator</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/crystal-coding-time" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com/crystal-coding-time" className="btn btn-primary" target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="A simple educational search tool" />
-          </div>
-          <h3>The Curious Amateur</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/crystal-coding-time" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com/crystal-coding-time" className="btn btn-primary" target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/crystal-coding-time" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com/crystal-coding-time" className="btn btn-primary" target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="Image of quizz project" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/crystal-coding-time" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com/crystal-coding-time" className="btn btn-primary" target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="Image of quizz project" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/crystal-coding-time" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com/crystal-coding-time" className="btn btn-primary" target='_blank'>Live Demo</a>
-          </div>
-        </article>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+                  <article key={id} className="portfolio__item">
+                    <div className="portfolio__item-image">
+                      <img src={image} alt={title} />
+                    </div>
+                    <h3>{title}</h3>
+                    <div className="portfolio__item-cta">
+                      <a href={github} className='btn' target='_blank'>Github</a>
+                      <a href={demo} className="btn btn-primary" target='_blank'>Live Demo</a>
+                    </div>
+                  </article>
+            )
+          }) 
+        }
+  
       </div>
     </section>
   )
---
+++ src/components/portfolio/portfolio-array.jsx
@@ -0,0 +1,53 @@
+import IMG1 from '../../assets/images/quizz.jpeg'
+import IMG2 from '../../assets/images/password.jpeg'
+import IMG3 from '../../assets/images/amature.jpeg'
+import IMG4 from '../../assets/images/amature.jpeg'
+import IMG5 from '../../assets/images/amature.jpeg'
+import IMG6 from '../../assets/images/amature.jpeg'
+
+const data = [
+    {
+        id: 1,
+        image: IMG1,
+        title: "JavaScript Quizz",
+        github: "https://github.com/crystal-coding-time",
+        demo: "https://github.com/crystal-coding-time",
+    },
+    {
+        id: 2,
+        image: IMG2,
+        title: "Password Generator",
+        github: "https://github.com/crystal-coding-time",
+        demo: "https://github.com/crystal-coding-time",
+    },
+    {
+        id: 3,
+        image: IMG3,
+        title: "The Curious Amateur",
+        github: "https://github.com/crystal-coding-time",
+        demo: "https://github.com/crystal-coding-time",
+    },
+    {
+        id: 4, 
+        image: IMG4,
+        title: "Bilbo Baggins",
+        github: "https://github.com/crystal-coding-time",
+        demo: "https://github.com/crystal-coding-time",
+    },
+    {
+        id: 5,
+        image: IMG5,
+        title: "Galadriel",
+        github: "https://github.com/crystal-coding-time",
+        demo: "https://github.com/crystal-coding-time",
+    },
+    {
+        id: 6,
+        image: IMG6,
+        title: "Gandalf",
+        github: "https://github.com/crystal-coding-time",
+        demo: "https://github.com/crystal-coding-time",
+    },
+]
+
+export default data
