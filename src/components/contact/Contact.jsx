e9d51a8fd889024a6bf4544bc033e94d251bdf11
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2023-04-12 13:21:25 -0400

    added CSS styling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,26 +13,31 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <TfiEmail />
+            <TfiEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>test@test.com</h5>
             <a href="mailto:test@test.com">Send An Email</a>
           </article>
           <article className="contact__option">
-            <AiFillMessage />
+            <AiFillMessage className='contact__option-icon'/>
             <h4>Messenger</h4>
             <h5>Facebook</h5>
             <a href="https://m.me/eliterkeurst" target='_blank'>Send A Message</a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp />
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>Whatsapp</h4>
             <h5>+19047750496</h5>
-            <a href="https://api.whatsapp.com/send?phone=+19047750496">Send A Text</a>
+            <a href="https://api.whatsapp.com/send?phone=+19047750496" target='_blank'>Send A Text</a>
           </article>
         </div>
         {/* End Of Contact Options */}
-        <form action=""></form>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required/>
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
     </section>
   )
